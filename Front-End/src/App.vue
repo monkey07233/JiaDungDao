271eed6a8de05ccbdf859604717d86176610fc80
whattiffany <1102b004@gmail.com>
2020-04-13 15:41:14 +0800

    new index

--- Front-End/src/App.vue
+++ Front-End/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app">    
     <router-view/>
   </div>
 </template>
 
 <script>
+import "@/assets/css/layout.css";
 export default {
   name: 'App'
 }
@@ -14,10 +14,5 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ Front-End/src/pages/index.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+    <div v-for="n in 10" class="col-3">
+        {{ n }}
+
+    </div>
+  </div>
+</template>
+
+<script>
+// import navbar from '@/components/navbar'
+import Vue from "vue";
+import "@/assets/css/index.css";
+export default {};
+</script>

dfab4a66779aa37c6b98df77b8885d87fd513fe5
chenyaya322 <49707398+chenyaya322@users.noreply.github.com>
2020-04-13 15:37:48 +0800

    navbar

---
+++ Front-End/src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">JiaDungDao</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">
+            Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+      </ul>
+      <div class="ml-auto">
+        <a href="#"><i class="fas fa-sign-in-alt"></i>Login</a>
+      </div>
+    </div>
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>>

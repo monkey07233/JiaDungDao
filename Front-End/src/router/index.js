af96a861d1a488fd3c22e97f7237fdbf48640f60
cyt <tim376755000@gmail.com>
2020-04-15 16:30:23 +0800

    profile ver1

--- Front-End/src/router/index.js
+++ Front-End/src/router/index.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/Index'
+import Profile from '@/pages/profile'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: "history",
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index
-    }
-  ]
+    mode: "history",
+    routes: [{
+            path: '/',
+            name: 'Index',
+            component: Index
+        },
+        {
+            path: '/Profile',
+            name: 'Profile',
+            component: Profile
+        }
+    ]
 })

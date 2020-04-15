e05bef765c90c71d98f09e03ef4d062d06de9175
cyt <tim376755000@gmail.com>
2020-04-15 16:45:47 +0800

    temp profile page

--- Front-End/src/pages/profile.vue
+++ Front-End/src/pages/profile.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container class="bv-example-row">
     <b-row>
-      <div class="col-sm-6">
+      <div class="col-sm-4">
         <img class="card-img-top" src="../assets/images/restaurant.jpg" />
 
         <b-card-body>
@@ -9,16 +9,16 @@
         </b-card-body>
 
         <b-list-group flush>
-          <b-list-group-item>E-mail:</b-list-group-item>
-          <b-list-group-item>生日:</b-list-group-item>
-          <b-list-group-item>住址:</b-list-group-item>
+          <b-list-group-item>生日: 87/08/07</b-list-group-item>
+          <b-list-group-item>E-mail: tiffany@example.com</b-list-group-item>
+          <b-list-group-item>住址: 鬼之王冠路77弄386巷666號</b-list-group-item>
         </b-list-group>
 
         <b-card-body>
           <a href="#" class="card-link">編輯</a>
           <!-- <a href="#" class="card-link">Another link</a> -->
         </b-card-body>
-        
+
       </div>
     </b-row>
   </b-container>

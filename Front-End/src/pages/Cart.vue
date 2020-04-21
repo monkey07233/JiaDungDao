6387be4d70be2f45e918785c109515e90d5a7080
chenyaya322 <heien40529@gmail.com>
2020-04-21 17:00:30 +0800

    Update Cart.vue

--- Front-End/src/pages/Cart.vue
+++ Front-End/src/pages/Cart.vue
@@ -1,16 +1,48 @@
 <template>
   <div class="container rounded bg-white mb-4">
-      <div class="row p-3">
-          <h4><font-awesome-icon icon="shopping-bag"/>&nbsp購物清單</h4>
-      </div>
-    <div class="row p-3">
-      <b-table hover :fields="fields" :items="items">
+    <div class="row ml-3 mr-3 pt-3">
+      <h4>
+        <font-awesome-icon icon="shopping-bag" />&nbsp購物清單
+      </h4>
+    </div>
+    <div class="row mt-3 ml-3 mr-3">
+      <b-table hover :fields="fields" :items="items" outlined>
         <template v-slot:cell(count)="data">
-          <b-button pill size="sm" variant="outline-secondary"><font-awesome-icon icon="minus" /></b-button>
+          <b-button pill size="sm" variant="outline-secondary">
+            <font-awesome-icon icon="minus" />
+          </b-button>
           &nbsp{{ data.item.count}}&nbsp
-          <b-button pill size="sm"  variant="outline-danger"><font-awesome-icon icon="plus" /></b-button>
+          <b-button pill size="sm" variant="outline-danger">
+            <font-awesome-icon icon="plus" />
+          </b-button>
         </template>
       </b-table>
+    </div>
+    <div class="row mb-3">
+      <div class="col-10">
+        <h5 class="text-right mr-3">訂單總金額</h5>
+      </div>
+      <div class="col-2 totalPrice">${{TotalPrice}}</div>
+    </div>
+    <div class="row p-3">
+      <div class="col-6">
+        <router-link
+          class="btn btn-secondary btn-block"
+          style="color:white;text-decoration:none;"
+          to="/"
+        >
+          <font-awesome-icon icon="store" />&nbsp繼續購物
+        </router-link>
+      </div>
+      <div class="col-6">
+        <router-link
+          class="btn btn-info btn-block"
+          style="color:white;text-decoration:none;"
+          to="/"
+        >
+          <font-awesome-icon icon="credit-card" />&nbsp結帳
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -19,11 +51,12 @@
 export default {
   data() {
     return {
+      TotalPrice: 0,
       fields: [
-        { key: "m_item", label: "餐點名稱"},
-        { key: "count", label: "數量" ,class:"text-center"},
-        { key: "m_price", label: "單價",class:"text-center" },
-        { key: "totalPrice", label: "小計",class:"text-center" }
+        { key: "m_item", label: "餐點名稱" },
+        { key: "count", label: "數量", class: "text-center" },
+        { key: "m_price", label: "單價", class: "text-center" },
+        { key: "totalPrice", label: "小計", class: "text-center" }
       ],
       items: [
         { m_item: "招牌大漢堡", count: 1, m_price: 12, totalPrice: 12 },
@@ -34,3 +67,9 @@ export default {
   }
 };
 </script>
+
+<style>
+.totalPrice {
+  color: red;
+}
+</style>

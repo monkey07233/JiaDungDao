56ebf5f5cacb767dbacca9e7a0e79c3483140d2d
Daniel870608 <aibity870608@gmail.com>
2020-04-21 13:42:39 +0800

    取得餐廳詳細資料包含菜單

--- Back-End/Repositories/RestaurantRepo.cs
+++ Back-End/Repositories/RestaurantRepo.cs
@@ -13,6 +13,29 @@ namespace Back_End.Repositories
         {
             this.db = dbContext;
         }
+
+        public List<Menu> GetAllMenuById(int Id)
+        {
+            var allMenu = db.Menu.Where(m => m.RestaurantID == Id).OrderBy(m => m.m_type).ToList();
+            if (allMenu != null)
+            {
+                return allMenu;
+            }else{
+                return null;
+            }
+        }
+
+        public List<string> GetAllMenuTypeById(int Id)
+        {
+            var allMenuType = db.Menu.Where(m => m.RestaurantID == Id).OrderBy(m => m.m_type).Select(m => m.m_type).Distinct().ToList();
+            if (allMenuType != null)
+            {
+                return allMenuType;
+            }else{
+                return null;
+            }
+        }
+
         public List<Restaurant> GetAllRestaurant()
         {
             var result = db.Restaurant.ToList();
@@ -23,5 +46,17 @@ namespace Back_End.Repositories
                 return null;
             }
         }
+
+        public Restaurant GetRestaurantById(int Id)
+        {
+            var result = db.Restaurant.Where(r => r.RestaurantID == Id).FirstOrDefault();
+            if (result != null)
+            {
+                return result;
+            }else{
+                return null;
+            }
+        }
+
     }
 }

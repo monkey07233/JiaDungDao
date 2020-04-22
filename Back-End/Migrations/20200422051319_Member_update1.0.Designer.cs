9bc3916c8ae19a4090c3c529fed874f2d2ba05b8
LU <monkey07233@gmail.com>
2020-04-22 13:18:31 +0800

    add table column role and account

--- Back-End/Migrations/20200422051319_Member_update1.0.Designer.cs
+++ Back-End/Migrations/20200422051319_Member_update1.0.Designer.cs
@@ -4,14 +4,16 @@ using JiaDungDao.Connection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JiaDungDao.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200422051319_Member_update1.0")]
+    partial class Member_update10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

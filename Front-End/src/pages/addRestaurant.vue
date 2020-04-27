134376e9dcf6dd813537e20e4d13a1c78aa001f4
whattiffany <1102b004@gmail.com>
2020-04-27 11:30:46 +0800

    編輯餐廳及新增餐廳頁面加入返回按鈕

--- Front-End/src/pages/addRestaurant.vue
+++ Front-End/src/pages/addRestaurant.vue
@@ -1,41 +1,46 @@
 <template>
-  <div class="container rounded bg-white mb-4">
-    <div class="row ml-3 mr-3 pt-3">
-      <h4 class="form-title">
-        <font-awesome-icon icon="plus" />&nbsp;新增餐廳
-      </h4>
+  <div class="container">
+    <b-button class="mb-4 font-weight-bold" @click="back" variant="warning">回到上一頁</b-button>
+    <div class="container rounded bg-white mb-4">
+      <div class="row ml-3 mr-3 pt-3">
+        <h4 class="form-title">
+          <font-awesome-icon icon="plus" />&nbsp;新增餐廳
+        </h4>
+      </div>
+      <hr />
+      <div class="row">
+        <b-form class="col-12" @submit.prevent="createRestaurant">
+          <b-form-group label="餐廳名稱" label-for="input-1">
+            <b-form-input
+              id="input-1"
+              type="text"
+              v-model="newRestaurant.r_name"
+              placeholder="請輸入餐廳名稱"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group label="餐廳地址：" label-for="input-2">
+            <b-form-input
+              id="input-2"
+              type="text"
+              v-model="newRestaurant.r_address"
+              placeholder="請輸入餐廳地址"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group label="聯絡電話" label-for="input-3">
+            <b-form-input
+              id="input-3"
+              type="tel"
+              v-model="newRestaurant.r_tel"
+              placeholder="請輸入餐廳聯絡電話"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-button type="submit" variant="primary" class="submit-btn">送出</b-button>
+        </b-form>
+      </div>
     </div>
-    <hr />
-    <b-form @submit.prevent="createRestaurant">
-      <b-form-group label="餐廳名稱" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          type="text"
-          v-model="newRestaurant.r_name"
-          placeholder="請輸入餐廳名稱"
-          required
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group label="餐廳地址：" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          type="text"
-          v-model="newRestaurant.r_address"
-          placeholder="請輸入餐廳地址"
-          required
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group label="聯絡電話" label-for="input-3">
-        <b-form-input
-          id="input-3"
-          type="tel"
-          v-model="newRestaurant.r_tel"
-          placeholder="請輸入餐廳聯絡電話"
-          required
-        ></b-form-input>
-      </b-form-group>
-      <b-button type="submit" variant="primary" class="submit-btn">送出</b-button>
-    </b-form>
   </div>
 </template>
 
@@ -73,6 +78,9 @@ export default {
           this.$router.push("/RestaurantManagement");
         }, 1000);
       });
+    },
+    back() {
+      this.$router.back();
     }
   }
 };

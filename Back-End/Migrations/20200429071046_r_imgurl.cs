bbd6e9f065ec306e3c19c704e4b0bca66cc4f304
cyt <tim376755000@gmail.com>
2020-04-29 15:27:39 +0800

    upload imgUrl to db

---
+++ Back-End/Migrations/20200429071046_r_imgurl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JiaDungDao.Migrations
+{
+    public partial class r_imgurl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "r_imgUrl",
+                table: "Restaurant",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "r_imgUrl",
+                table: "Restaurant");
+        }
+    }
+}

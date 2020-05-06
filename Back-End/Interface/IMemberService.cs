bd5e68a04d9a0f91da0b75b2e7f6ff41a0b551ad
Yoga Lai <5659274-yoga.lai@users.noreply.gitlab.com>
2020-05-06 15:20:24 +0800

    verify application v2

--- Back-End/Interface/IMemberService.cs
+++ Back-End/Interface/IMemberService.cs
@@ -20,7 +20,7 @@ namespace Back_End.Interface
          bool updateMemberImgUrl(int Id);
          bool ApplyResAdmin(Application apply);
          List<Member> GetAllMember();
-         bool VerifyApplication(bool pass,string account);
+         bool VerifyApplication(bool pass,int applicationId);
 
     }
 }
